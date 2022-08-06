1634e296261a8706561a5ed7c2122f6e4dd99b2e
stamovm <m.stamov@yahoo.com>
2022-08-05 23:51:21 -0700

    Get random pokemon

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,19 +1,23 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
 import { trpc } from '../utils/trpc'
+import { getOptionsForVote } from '../utils/getRandomPokemon'
 
 const Home: NextPage = () => {
   const hello = trpc.useQuery(['example.hello', { text: 'from tRPC' }])
-  console.log('hello:' hello)
+  const [first, second] = getOptionsForVote()
+
+  console.log(hello?.data?.greeting)
+
   return (
     <div className="flex flex-col items-center justify-center w-screen h-screen">
-      <div className="p-8">{hello.data.greeting}</div>
+      <div className="p-8">{hello?.data?.greeting}</div>
       <div className="text-2xl text-center">Which Pokemon looks stronger?</div>
       <div className="p-2" />
       <div className="flex items-center justify-between max-w-2xl p-8 border rounded">
-        <div className="w-16 h-16 bg-teal-200" />
+        <div className="w-16 h-16 bg-teal-200">{first} </div>
         <div className="p-8">Vs</div>
-        <div className="w-16 h-16 bg-teal-200" />
+        <div className="w-16 h-16 bg-teal-200"> {second} </div>
       </div>
     </div>
   )
